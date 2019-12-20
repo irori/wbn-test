26b529b1896a7e323bf7509a1a393c14c0ef5ec6
Kunihiko Sakamoto <ksakamoto@chromium.org>
2019-12-20 10:47:04 +0900

    Add iframe test

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ site=wbn-test.web.app
 
 mkdir -p $outdir
 
-for dir in hello
+for dir in hello iframe
 do
     url=https://$site/$dir/
     gen-bundle -version b1 -primaryURL $url -baseURL $url -dir $dir -o $outdir/$dir.wbn
